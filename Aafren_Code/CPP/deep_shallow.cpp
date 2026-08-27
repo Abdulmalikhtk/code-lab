2463a12b350535073ebe8e5cf269a068e1be142e
Aafren Fathima M <aafrenfathimam@meta.com>
2026-08-27 22:30:46 +0530

    print

--- Aafren_Code/CPP/deep_shallow.cpp
+++ Aafren_Code/CPP/deep_shallow.cpp
@@ -20,11 +20,13 @@ class Test
 
   Test(const Test& t,bool shallow)
   {
+    cout<<"Shallow copy------------"<<endl;
       age=t.age;
       name=t.name;
   }
   Test(const Test& t)
   {
+    cout<<"Deep copy-------------"<<endl;
       age=t.age;
       name=new string(*(t.name));
   }
@@ -34,29 +36,40 @@ class Test
   }
 };
 int main() {
+    cout<<"t1 object----------"<<endl;
     Test t1(27,"Charlie");
     t1.show();
+    cout<<"----------------------------------------"<<endl;
 
+    cout<<"t2 object----------------"<<endl;
     Test t2(30,"Daniel");
     t2.show();
+    cout<<"----------------------------------------"<<endl;
 
+    cout<<"t3 object deep copy-----------"<<endl;
     Test t3(t1);
     t3.show();
-	t2.show();
     t1.show();
+    cout<<"----------------------------------------"<<endl;
 
+    cout<<"t4 object shallow copy------"<<endl;
     Test t4(t2,true);
 
     t4.show();
     t2.show();
+    cout<<"----------------------------------------"<<endl;
 
+    cout<<"t4 object name change shallow copy------"<<endl;
     *(t4.name)="bob";
     t4.show();
     t2.show();
+    cout<<"----------------------------------------"<<endl;
 
+    cout<<"t2 object name change deep copy------"<<endl;
     *(t3.name)="alice";
     t3.show();
     t1.show();
+    cout<<"----------------------------------------"<<endl;
 
 
     cout<<"t1 and t2 are "<<(t1==t2? "equal":"Not equal")<<endl;
